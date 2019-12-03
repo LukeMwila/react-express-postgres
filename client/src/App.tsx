2dc8773eec485561d240aaf53906baac4714a067
Lukonde Francis Mwila <lukonde.mwila@entelect.co.za>
2019-12-03 13:28:37 +0200

    Updated docker config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,37 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div>App working</div>;
+  const [toDoItems, updateToDoItems] = React.useState([]);
+
+  React.useEffect(() => {
+    const getToDoItems = async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_TO_DO_ITEMS_API}/items`
+      );
+
+      const items = await response.json();
+      if (items && Array.isArray(items) && items.length) {
+        // @ts-ignore
+        updateToDoItems(items);
+      }
+    };
+    getToDoItems();
+  }, []);
+
+  return (
+    <div>
+      {toDoItems && toDoItems.length
+        ? toDoItems.map((item: any, i: number) => {
+            return (
+              <div key={i}>
+                {`${item.item_name}`}
+                <br />
+              </div>
+            );
+          })
+        : 'No items to be done'}
+    </div>
+  );
 }
 
 export default App;
